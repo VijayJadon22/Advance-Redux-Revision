95983e133cb76ec4da2fc0bd34653370a9616fbd
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2025-04-11 00:16:19 +0530

    Implemnted Home and Products component

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,18 +1,25 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import ProductCard from "./ProductCard";
+import { fetchProducts, STATUSES } from "../store/productSlice.js";
+import { useSelector, useDispatch } from "react-redux";
 
 const Products = () => {
-  const [products, setProducts] = useState([]);
+  //   const [products, setProducts] = useState([]);
+  const { data: products, status } = useSelector((state) => state.product);
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    const fetchProducts = async () => {
-      const res = await fetch("https://fakestoreapi.com/products/");
-      const data = await res.json();
-      setProducts(data);
-    };
-
-    fetchProducts();
-  }, []);
+    // const fetchProducts = async () => {
+    //   const res = await fetch("https://fakestoreapi.com/products/");
+    //   const data = await res.json();
+    //   setProducts(data);
+    // };
+    // fetchProducts();
+    dispatch(fetchProducts());
+  }, [dispatch]);
+    
+  if (status === "loading") return <p>Loading...</p>;
+  if (status === "error") return <p>Error fetching products</p>;
   return (
     <div className="productsWrapper">
       {products.map((product) => (
