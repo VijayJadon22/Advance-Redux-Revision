4a9ae58aec8a850de5e5d787447f70ffbe189d1a
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2025-04-10 22:53:23 +0530

    Implemnted NavBar and accessed the cart items length using useSelector

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,17 +1,27 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
 const NavBar = () => {
+  const cart = useSelector((state) => state.cart);
   return (
-    <div style={{
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'space-between',
-    }}>
-      <span className="logo">Redux Store</span>
-      <Link className="navLink" to={"/"}>Home</Link>
-      <Link className="navLink" to={"/cart"}>Cart</Link>
-      <span className="cartCount">Cart items: 0</span>
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "space-between",
+      }}
+    >
+      <span className="logo">REDUX STORE</span>
+      <div>
+        <Link className="navLink" to="/">
+          Home
+        </Link>
+        <Link className="navLink" to="/cart">
+          Cart
+        </Link>
+        <span className="cartCount">Cart items: {cart?.length}</span>
+      </div>
     </div>
   );
 };
