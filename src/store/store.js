562d97fe51f4a49c4184b6e9b801b9a1f2eaf5ba
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2025-04-11 00:15:53 +0530

    Implemnted store using configureStore and added the reducer of the carta nd product features

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "./cartSlice.js";
+import productReducer from "./productSlice.js";
 
 const store = configureStore({
     reducer: {
-        cart: cartReducer
+        cart: cartReducer,
+        product: productReducer,
     }
 });
 
